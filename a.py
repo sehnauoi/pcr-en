c18df2e8f8fec70c797630fd14aaf47467520b40
sehnauoi <105583853+sehnauoi@users.noreply.github.com>
2022-06-02 22:02:24 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-import requests
 import pandas as pd
-import os
-
-bearer_token = os.environ.get('BEARER_TOKEN')
-headers = {"Authorization": "Bearer {}".format(bearer_token)}
-
-url = "https://raw.githubusercontent.com/Expugn/priconne-quest-helper/master/data/character_data.json"
-response = requests.request("GET", url, headers=headers).json()
-
-df = pd.DataFrame(response['data'])
-df.to_csv('a.csv')
+df = pd.read_json (r'character_data.json')
+df.to_csv (r'character_data.csv')
